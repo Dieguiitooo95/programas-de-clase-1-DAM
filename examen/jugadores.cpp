489c143581f92cc9929ef4da16ba907471aec747
Diego <diego@ubuntu.(none)>
2015-02-22 22:31:08 +0100

    jugadores

--- examen/jugadores.cpp
+++ examen/jugadores.cpp
@@ -1,26 +1,69 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
+#define A 10
+#define B 0x100
+#define FICHERO "numeros.bin"
+#define NOMBRE jugador[i].nombre
+#define FUERZA jugador[i].habilidad.fuerza
+#define RESISTENCIA jugador[i].habilidad.resistencia
+#define VELOCIDAD jugador[i].habilidad.velocidad
+#define DINERO jugador[i].dinero
+#define PUNTOS jugador[i].puntos
 
-    struct TInfo {
-	char Nombre[0X20];
+struct Habilidades {
+    int fuerza;
+    int resistencia;
+    int velocidad;
+};
 
+struct TInfo{
+    char nombre[B];
+    Habilidades habilidad;
+    int puntos;
+    int dinero;
+};
 
-	/*Arraid de 10 jugadores(cada celda es de tipo jugador)
-	 *
-	 * 2 structs:
-	 * {Nombre
-	 * habilidad(struct)
-	 *    fuerza
-	 *    resistencia
-	 *    velocidad
-	 * puntos
-	 * dinero
-	 * }(struct)
-	 */
-
-	FILE *pdatos;
-
-	pdatos = fopen("datos.txt", "rb");
+void rellena (struct TInfo jugador[A]){
+    for(int i=0; i<A; i++){
+	system("clear");
+	system("figlet JUGADORES");
+	printf("Introduce el nombre del jugador %i:\n", i+1);
+	scanf("%s", NOMBRE);
+	printf("Introduce la fuerza(0-10): ");
+	scanf("%i", &FUERZA);
+	printf("Introduce la resistencia(0-10): ");
+	scanf("%i", &RESISTENCIA);
+	printf("Introduce la velocidad(0-10): ");
+	scanf("%i", &VELOCIDAD);
+	printf("Introduce puntos: ");
+	scanf("%i", &PUNTOS);
+	printf("Introduce dinero recaudado: ");
+	scanf("%i", &DINERO);
     }
+}
+
+void guarda (struct TInfo jugador[A],FILE *puntero){
+    for(int i=0; i<A; i++){
+	fwrite(NOMBRE, sizeof(char), B, puntero);
+	fwrite(&FUERZA, sizeof(int), 2, puntero);
+	fwrite(&RESISTENCIA, sizeof(int), 2, puntero);
+	fwrite(&VELOCIDAD, sizeof(int), 2, puntero);
+	fwrite(&PUNTOS, sizeof(int), 2, puntero);
+	fwrite(&DINERO, sizeof(int), 2, puntero);
+    }
+}
+
+int main () {
+    FILE *numeros;
+    struct TInfo jugador[A];
+    if((numeros = fopen(FICHERO, "rb")) == NULL){
+	numeros = fopen(FICHERO, "wb");
+	rellena(jugador);
+	guarda(jugador, numeros);
+    }else {
+	fread(jugador, sizeof(struct TInfo), A, numeros);
+    }
+    fclose(numeros);
+    return EXIT_SUCCESS;
+}
